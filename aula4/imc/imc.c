5bba183ee29984b11f20405f84f8c77476218de1
Hendrik Vinicius <hendrikvinicius.hv@gmail.com>
2021-03-01 11:50:23 -0300

    Acrescentei aula 1,2,3 e 4

---
+++ aula4/imc/imc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* 
+	Cálculo do IMC
+	@author Professor José de Assis
+*/
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"Portuguese");
+	system("color 1F"); 
+	printf("d888888b .88b  d88.  .o88b. \n");
+	printf("  `88'   88'YbdP`88 d8P  Y8 \n");
+	printf("   88    88  88  88 8P      \n");
+	printf("   88    88  88  88 8b      \n");
+	printf("  .88.   88  88  88 Y8b  d8 \n");
+	printf("Y888888P YP  YP  YP  `Y88P' \n");
+	printf("\n");
+	//variáveis
+	float imc,peso,altura;
+	//entrada
+	printf("Digite o seu peso em Kg: ");
+	scanf("%f",&peso);
+	printf("Digite a sua altura em metros: ");
+	scanf("%f",&altura);
+	//processamento
+	imc = peso / (altura * altura);
+	//saída
+	printf("IMC: %.2f\n",imc);
+	system("pause");
+	return 0;
+}
